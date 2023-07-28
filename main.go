2bda15b8e31fda85840421c58d69abefe42736a8
Bainfront <bainfront@gmail.com>
2023-07-28 17:51:14 +0600

    Lession cycle

--- main.go
+++ main.go
@@ -3,15 +3,55 @@ package main
 import "fmt"
 
 func main() {
+	// Array
+	//arr := [...]string{"Belka", "Up", "Down", "Break", "Absolut", "Drug"}
+	// Full Format
+	//for i := 0; i < 6; i++ {
 
-	var a bool = true    //Boolean (true/false)
-	var b int = 4        //Integer
-	var c float32 = 3.14 // Floating ponit number
-	var d string = "Big Boy"
+	//	fmt.Println(arr[i])
 
-	fmt.Println("Boealean = ", a)
-	fmt.Println("Integer = ", b)
-	fmt.Println("Float = ", c)
-	fmt.Println("String = ", d)
+	//}
+	// Alternative Format
+	//i := 0
+	//for i < 6 {
+	//		fmt.Println(arr[i])
+	//	i++
+	//}
+
+	// Infinite Loop
+	//for {
+	//	fmt.Println("Hello World")
+	//}
+
+	// Sum of numbers
+	//arr := [...]int{10, 15, 51, 95, 47, 84, 25, 63, 8, 74, 1, 952, 98}
+	//sum := 0
+	//for i := 0; i < len(arr); i++ {
+	//	sum += arr[i]
+	//}
+	//fmt.Println(sum)
+
+	//for i := 0; i < 10; i++ {
+	//
+	//	if i == 5 {
+
+	//		continue
+
+	//	}
+	//	fmt.Println(i)
+	//}
+
+	// Range
+	//arr := [...]int{10, 15, 51, 95, 47, 84, 25, 63, 8, 74, 1, 952, 98}
+	//	for i, val := range arr {
+	//
+	//	fmt.Println(i, val)
+	//
+	//}
+	// Slice
+	contacts := []string{"add", "see", "more"}
+	contacts2 := contacts[1:]
+
+	fmt.Println(contacts2)
 
 }
