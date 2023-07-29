8cc1ad129e220a8409d2172fe78e4431a8c1e0b8
Bainfront <bainfront@gmail.com>
2023-07-29 18:19:56 +0600

    absd

--- main.go
+++ main.go
@@ -1,57 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	// Array
-	//arr := [...]string{"Belka", "Up", "Down", "Break", "Absolut", "Drug"}
-	// Full Format
-	//for i := 0; i < 6; i++ {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	grade := random.Intn(5) + 10
 
-	//	fmt.Println(arr[i])
-
-	//}
-	// Alternative Format
-	//i := 0
-	//for i < 6 {
-	//		fmt.Println(arr[i])
-	//	i++
-	//}
-
-	// Infinite Loop
-	//for {
-	//	fmt.Println("Hello World")
-	//}
-
-	// Sum of numbers
-	//arr := [...]int{10, 15, 51, 95, 47, 84, 25, 63, 8, 74, 1, 952, 98}
-	//sum := 0
-	//for i := 0; i < len(arr); i++ {
-	//	sum += arr[i]
-	//}
-	//fmt.Println(sum)
-
-	//for i := 0; i < 10; i++ {
-	//
-	//	if i == 5 {
-
-	//		continue
-
-	//	}
-	//	fmt.Println(i)
-	//}
-
-	// Range
-	//arr := [...]int{10, 15, 51, 95, 47, 84, 25, 63, 8, 74, 1, 952, 98}
-	//	for i, val := range arr {
-	//
-	//	fmt.Println(i, val)
-	//
-	//}
-	// Slice
-	contacts := []string{"add", "see", "more"}
-	contacts2 := contacts[1:]
-
-	fmt.Println(contacts2)
+	// if grade == 1 {
+	// 	fmt.Println("Fall!")
+	// } else if grade == 2 {
+	// 	fmt.Println("Bad!")
+	// } else if grade == 3 {
+	// 	fmt.Println("Good!")
+	// } else if grade == 4 {
+	// 	fmt.Println("Nice!")
+	// } else if grade == 5 {
+	// 	fmt.Println("Excelent!")
+	// }
+	// switch
+	switch grade {
+	case 1:
+		fmt.Println("Fall!")
+	case 2:
+		fmt.Println("Bad!")
+	case 3:
+		fmt.Println("Good!")
+	case 4:
+		fmt.Println("Nice!")
+	case 5:
+		fmt.Println("Excelent!")
+	default:
+		fmt.Println("NONCENCE")
+	}
 
 }
