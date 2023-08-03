d1d39cb48f4152ecd5daa88fedc2de5e1a4cdc4b
Bainfront <bainfront@gmail.com>
2023-08-03 14:28:17 +0600

    aad

--- main.go
+++ main.go
@@ -2,40 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"strings"
 )
 
 func main() {
-	source := rand.NewSource(time.Now().UnixNano())
-	random := rand.New(source)
-	grade := random.Intn(5) + 10
+factResult := fact(5)
+fmt.Println(factResult)
+}
 
-	// if grade == 1 {
-	// 	fmt.Println("Fall!")
-	// } else if grade == 2 {
-	// 	fmt.Println("Bad!")
-	// } else if grade == 3 {
-	// 	fmt.Println("Good!")
-	// } else if grade == 4 {
-	// 	fmt.Println("Nice!")
-	// } else if grade == 5 {
-	// 	fmt.Println("Excelent!")
-	// }
-	// switch
-	switch grade {
-	case 1:
-		fmt.Println("Fall!")
-	case 2:
-		fmt.Println("Bad!")
-	case 3:
-		fmt.Println("Good!")
-	case 4:
-		fmt.Println("Nice!")
-	case 5:
-		fmt.Println("Excelent!")
-	default:
-		fmt.Println("NONCENCE")
-	}
-
+func fact (n int) int {
+if n ==0 (return 1)
+return n*fact(n -1)
 }
